e4b51659ba1176f7dd968923223d404f0ce7e162
devin <devins.d@protonmail.com>
2020-11-21 22:06:33 -0400

    desktop update

--- js/index.js
+++ js/index.js
@@ -4,45 +4,169 @@ Date: Novemeber 12th, 2020
 File: index.js
 */
 
+
+/*
+=====================================
+MINI LIBRARY
+=====================================
+*/
+
+const _ = (selector) => {
+    let element = document.querySelector(selector)
+    return {
+        children: () => {
+            return element.children
+        },
+        remove: () => {
+            element.remove()
+            return {
+                next: {
+
+                } 
+            }
+        },
+        class: {
+            add: (name) => {
+                    element.classList.add(name)
+            },
+            remove: (name) => {
+                    element.classList.remove(name)
+            },
+            list: () => {
+                return element.classList
+            }
+         },
+         hide: () => {
+            element.style.display = "none"
+         },
+        attr: (name, value) => {
+            element.setAttribute(name, value)
+        },
+
+        listen: (e, func) => {
+            element.addEventListener(e, func)
+        }
+        }
+        
+    }
+
+/*
+=====================================
+MOBILE NAV EVENT HANDLERS
+=====================================
+*/
+
+// When mobile nav item is pressed move the intro page cover off screen. 
 document.querySelectorAll(".open-btn").forEach(el => {
     el.addEventListener("click", (e) => {
         e.preventDefault();
-        document.querySelector(".main-nav").classList.add("back-nav");
-        document.querySelector(".air-lock").classList.add("lock-top");
-        document.querySelector(".back-btn").classList.add("back-btn-visible");
+        _(".main-nav").class.add("back-nav");
+        _(".doors").class.add("lock-top");
+        _(".back-btn").class.add("back-btn-visible");
     });
 });
 
-document.querySelector(".back-btn").addEventListener("click", () => {
-    document.querySelector(".main-nav").classList.remove("back-nav");
-    document.querySelector(".air-lock").classList.remove("lock-top");
-    document.querySelector(".back-btn").classList.remove("back-btn-visible");
-    document.querySelector(".about-me").style.display = "none"
-    document.querySelector(".contact-me").style.display = "none"
-    document.querySelector(".work").style.display = "none"
-
+// When mobile back btn is pressed move the intro page cover on screen. 
+_(".back-btn").listen("click", () => {
+    _(".main-nav").class.remove("back-nav");
+    _(".doors").class.remove("lock-top");
+    _(".back-btn").class.remove("back-btn-visible");
+    _(".about-me").attr("style", "display: none");
+    _(".contact-me").hide()
+    _(".work").hide()
 });
 
-document.querySelector("#about-btn").addEventListener("click", () => {
-    document.querySelector(".about-me").style.display = "grid"
+// When mobile about btn is pressed display about page.
+_("#mobile-about-btn").listen("click", () => {
+    _(".about-me").attr("style", "display: grid")
 });
 
-document.querySelectorAll(".contact-btn").forEach(btn => {
+// When mobile contact btn is pressed display contact page.
+document.querySelectorAll(".mobile-contact-btn").forEach(btn => {
     btn.addEventListener("click", () => {
-    document.querySelector(".contact-me").style.display = "grid"
-});
-});
-
-document.querySelector("#work-btn").addEventListener("click", () => {
-    document.querySelector(".work").style.display = "grid"
+        _(".contact-me").attr("style", "display: grid")
+    });
 });
 
+// When mobile contact btn is pressed display work page.
+_("#mobile-work-btn").listen("click", () => {
+    _(".work").attr("style", "display: grid")
+});
 
-// on window resize check if portrait
-window.addEventListener("resize", () => {
-    if(window.innerHeight < window.innerWidth) {
-        document.querySelector(".piano").classList.add("open-piano")
-    } else {
-        document.querySelector(".piano").classList.remove("open-piano")
+/*
+=====================================
+DESKTOP NAV EVENT HANDLERS
+=====================================
+*/
+
+const moveDoor = () => {
+    let door = _("#desktop-door")
+    if(door.class.list().contains("open-door")) {
+        door.class.remove("open-door")
+        door.class.add("close-door")
+    } 
+    setTimeout(() => {
+        door.class.add("open-door")
+        door.class.remove("close-door")
+    }, 500);
+}
+
+const typeItIn = (title, element) => {
+    let i, newTitle = ""
+    for(i = 0; i < title.length; i++) {
+        setTimeout(() => {
+            newTitle += title[i]
+            element.textContent = title
+        }, 200);
     }
-})
+}
+
+// When mobile about btn is pressed display about page.
+_("#about-btn").listen("click", (e) => {
+    e.preventDefault()
+        let title =  _("#intro-text").children()[0]
+        typeItIn("A little about me...", title)
+        moveDoor()
+        setTimeout(() => {
+            _(".about-me").attr("style", "display: grid");
+            _(".contact-me").hide();
+            _(".work").hide()
+        }, 520)
+});
+
+// When mobile contact btn is pressed display contact page.
+document.querySelectorAll(".contact-btn").forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault()
+            moveDoor()
+            _("#intro-text").children()[0].textContent = "Lets ge in touch!"
+            setTimeout(() => {
+                _(".contact-me").attr("style", "display: grid")
+                _(".work").hide()
+                _(".about-me").hide();
+            }, 520)
+      
+    });
+});
+
+// When mobile contact btn is pressed display work page.
+_("#work-btn").listen("click", (e) => {
+    e.preventDefault()
+        moveDoor()
+        _("#intro-text").children()[0].textContent = "This is some of my work..."
+        setTimeout(() => {
+            _(".work").attr("style", "display: grid")
+            _(".contact-me").hide();
+            _(".about-me").hide()
+        }, 520)
+});
+
+
+// // on window resize check if portrait
+// window.addEventListener("resize", () => {
+//     if(window.innerHeight < window.innerWidth) {
+//         document.querySelector(".piano").classList.add("open-piano")
+//     } else {
+//         document.querySelector(".piano").classList.remove("open-piano")
+//     }
+// });
